795e8983125baaef0543407974cb3b995cd90965
Jon Clayton <m87tech@gmail.com>
2021-02-19 17:00:11 +0000

    ansible module for ovh deployment

---
+++ plugins/modules/ovh_deploy.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import *
+import ovh
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        endpoint=dict(type="str", required=True),
+        application_key=dict(type="str", required=True),
+        application_secret=dict(type="str", required=True),
+        consumer_key=dict(type="str", required=True),
+        server_inventory=dict(type="list", required=True),
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(changed=False, original_message="", message="")
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    # reinstall_servers
+    reinstall_servers(module)
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+
+def reinstall_servers(module):
+    secrets = get_secrets(module)
+    client = get_client(secrets)
+    server_inventory = module.params["server_inventory"]
+    # re-install servers
+    for server in server_inventory:
+        friendly_name = server["friendly_name"]
+        retreived_hostname = server["hostname"]
+        start_install(client, server=retreived_hostname, friendly_name=friendly_name)
+
+
+def get_secrets(module) -> dict:
+    ovh_secrets = {}
+    ovh_secrets["endpoint"] = module.params["endpoint"]
+    ovh_secrets["application_key"] = module.params["application_key"]
+    ovh_secrets["application_secret"] = module.params["application_secret"]
+    ovh_secrets["consumer_key"] = module.params["consumer_key"]
+    return ovh_secrets
+
+
+def get_client(secrets):
+    ovh_secrets = secrets
+    client = ovh.Client(
+        endpoint=ovh_secrets["endpoint"],
+        application_key=ovh_secrets["application_key"],
+        application_secret=ovh_secrets["application_secret"],
+        consumer_key=ovh_secrets["consumer_key"],
+    )
+    return client
+
+
+def start_install(client, server, friendly_name):
+
+    # create details  type
+    details = {
+        "customHostname": friendly_name,
+        "noRaid": True,
+        "resetHwRaid": True,
+        "sshKeyName": "troon",
+    }
+
+    result = client.post(
+        f"/dedicated/server/{server}/install/start",
+        partitionSchemeName="jons-partition-latest",  # Partition scheme name (type: string)
+        templateName="wiz-debian-10",  # Template name (type: string)
+        details=details,  # parameters for default install (complex type: dedicated.server.InstallCustom)
+    )
+
+    return result
+
+
+def main():
+    run_module()
+
+
+if __name__ == "__main__":
+    main()
--- roles/ovh/files/ovh_deploy.py
+++ roles/ovh/files/ovh_deploy.py
@@ -1,55 +1,21 @@
 import json
+import sys
 import ovh
+from ovh.client import ENDPOINTS
 import yaml
 import os
 import pytest
 
 
-def initialize():
-    try:
-        my_input = input
-    except NameError:
-        pass
-
-    client = get_client()
-
-    # Request RO, /me API access
-    ck = client.new_consumer_key_request()
-    # ck.add_rules(ovh.API_READ_ONLY, "/me")
-    ck.add_recursive_rules(ovh.API_READ_WRITE, "/")
-
-    # Request token
-    validation = ck.request()
-
-    print("Please visit %s to authenticate" % validation["validationUrl"])
-    input("and press Enter to continue...")
-
-    # Print nice welcome message
-    print("Welcome", client.get("/me")["firstname"])
-    print("Btw, your 'consumerKey' is '%s'" % validation["consumerKey"])
-
-
-def get_ovh_secrets():
-
-    dir = os.path.dirname(os.path.realpath(__file__))
-    full_path = dir + "/" + ".ovh_module_vars.yml"
-    with open(full_path) as file:
-        secrets_file = yaml.load(file, Loader=yaml.FullLoader)
-    ovh_secrets = secrets_file["ovh"]
-
-    return ovh_secrets
-
-
-def get_server_inventory() -> list:
-
-    dir = os.path.dirname(os.path.realpath(__file__))
-    full_path = dir + "/" + ".ovh_module_vars.yml"
-    with open(full_path) as file:
-        secrets_file = yaml.load(file, Loader=yaml.FullLoader)
-
-    server_inventory = secrets_file["ovh"]["servers"]
-
-    return server_inventory
+# def get_ovh_secrets():
+#
+#    dir = os.path.dirname(os.path.realpath(__file__))
+#    full_path = dir + "/" + ".ovh_module_vars.yml"
+#    with open(full_path) as file:
+#        secrets_file = yaml.load(file, Loader=yaml.FullLoader)
+#    ovh_secrets = secrets_file["ovh"]
+#
+#    return ovh_secrets
 
 
 def get_client_auth():
@@ -68,143 +34,6 @@ def get_client_auth():
     )
 
     return result
-
-
-def get_client():
-
-    ovh_secrets = get_ovh_secrets()
-
-    client = ovh.Client(
-        endpoint=ovh_secrets["endpoint"],
-        application_key=ovh_secrets["application_key"],
-        application_secret=ovh_secrets["application_secret"],
-        consumer_key=ovh_secrets["consumer_key"],
-    )
-
-    return client
-
-
-def get_services(client):
-    services = client.get("/dedicated/server")
-    return services
-
-
-def get_service_details(client, service):
-    service_details = client.get(f"/dedicated/server/{service}")
-    return service_details
-
-
-def get_templates(client):
-    templates = client.get("/dedicated/installationTemplate")
-    return templates
-
-
-def get_my_templates(client):
-    templates = client.get("/me/installationTemplate")
-    return templates
-
-
-@pytest.mark.ovh
-@pytest.mark.ovh_get_partition_schemes
-def get_partition_schemes(client=get_client(), template_name="wiz-debian-10"):
-    pscheme = client.get(f"/me/installationTemplate/{template_name}/partitionScheme")
-    print_result(pscheme)
-    return pscheme
-
-
-@pytest.mark.ovh
-@pytest.mark.ovh_get_partition_scheme
-def get_partition_scheme(
-    client=get_client(),
-    template_name="wiz-debian-10",
-    scheme_name="jons-partition-latest",
-):
-    pscheme = client.get(
-        f"/me/installationTemplate/{template_name}/partitionScheme/{scheme_name}"
-    )
-    print_result(pscheme)
-    return pscheme
-
-
-@pytest.mark.ovh_get_partitions
-def get_partitions(
-    client=get_client(),
-    template_name="wiz-debian-10",
-    scheme_name="jons-partition-latest",
-):
-    pscheme = client.get(
-        f"/me/installationTemplate/{template_name}/partitionScheme/{scheme_name}/partition"
-    )
-    print_result(pscheme)
-    return pscheme
-
-
-@pytest.mark.ovh_add_partition
-def add_partition_scheme(client=get_client(), template_name="wiz-debian-10"):
-    p = client.post(
-        f"/me/installationTemplate/{template_name}/partitionScheme",
-        name="jons-partition-latest",
-        priority=10,
-    )
-    print_result(p)
-    return
-
-
-@pytest.mark.ovh_add_partition_hardware_raid
-def add_partition_scheme_hwraid(
-    client=get_client(),
-    template_name="wiz-debian-10",
-    scheme_name="jons-partition-latest",
-):
-    p = client.post(
-        f"/me/installationTemplate/{template_name}/partitionScheme/{scheme_name}/partition",
-        filesystem="ext4",
-        mountpoint="/",
-        size=120000,
-        step=1,
-        type="primary",
-    )
-    print_result(p)
-    return
-
-
-@pytest.mark.ovh_add_partition_hardware_raid_swap
-def add_partition_scheme_hwraid_swap(
-    client=get_client(),
-    template_name="wiz-debian-10",
-    scheme_name="jons-partition-latest",
-):
-    p = client.post(
-        f"/me/installationTemplate/{template_name}/partitionScheme/{scheme_name}/partition",
-        filesystem="swap",
-        mountpoint="/swap",
-        size=4000,
-        step=2,
-        type="primary",
-    )
-    print_result(p)
-    return
-
-
-def get_disk_groups(client, service):
-    disk_groups = client.get(f"/dedicated/server/{service}/specifications/hardware")
-    return disk_groups
-
-
-@pytest.mark.ovh
-@pytest.mark.ovh_get_status
-def get_install_status():
-
-    client = get_client()
-    server_inventory = get_server_inventory()
-
-    for item in server_inventory:
-        service_name = item["hostname"]
-        try:
-            status = client.get(f"/dedicated/server/{service_name}/install/status")
-            print_result(status)
-        except:
-            print("Not installing ")
 
 
 def start_install(client, server, friendly_name):
@@ -252,32 +81,71 @@ def reinstall_servers():
         start_install(client, server=retreived_hostname, friendly_name=friendly_name)
 
 
-def main():
+def main(*args):
+    o = OvhDeploy(*args)
 
-    client = get_client()
-
-    results = {
-        "services": get_services(client),
-        "service_details": get_service_details(client),
-        "templates": get_templates(client),
-        "my_templates": get_my_templates(client),
-        "partition_schemes": get_partition_schemes(client),
-        "partitions": get_partitions(client),
-        "disk_groups": get_disk_groups(client),
-    }
-
-    # install_results = {
-    #    "installation": start_install(client, servers=get_services(client))
-    # }
-
-    print_results(results)
+    o.reinstall_servers()
 
 
 # main()
 # run initialize to get consumer key
 # initialize()
 # run reinstall servers to reinstall all servers
-reinstall_servers()
 
 # run get install status to get progress of reinstallation
 # get_install_status()
+
+
+class OvhDeploy:
+    """
+    Deploy baremetal via ovh API
+    """
+
+    COMMON_SSH_ARGS = "-o StrictHostKeyChecking=no"
+
+    def __init__(self, *args):
+        self.endpoint = sys.argv[1]
+        self.application_key = sys.argv[2]
+        self.application_secret = sys.argv[3]
+        self.consumer_key = sys.argv[4]
+        self.server_inventory = sys.argv[5]
+
+    def get_secrets(self) -> dict:
+
+        ovh_secrets = {}
+        ovh_secrets["endpoint"] = self.endpoint
+        ovh_secrets["application_key"] = self.application_key
+        ovh_secrets["application_secret"] = self.application_secret
+        ovh_secrets["consumer_key"] = self.consumer_key
+
+        return ovh_secrets
+
+    def reinstall_servers(self):
+        secrets = self.get_secrets()
+        client = self.get_client(secrets)
+        server_inventory = self.server_inventory
+
+        # re-install servers
+        for server in server_inventory:
+            friendly_name = server["friendly_name"]
+            retreived_hostname = server["hostname"]
+            start_install(
+                client, server=retreived_hostname, friendly_name=friendly_name
+            )
+
+    def get_client(self, secrets):
+
+        ovh_secrets = secrets
+
+        client = ovh.Client(
+            endpoint=ovh_secrets["endpoint"],
+            application_key=ovh_secrets["application_key"],
+            application_secret=ovh_secrets["application_secret"],
+            consumer_key=ovh_secrets["consumer_key"],
+        )
+
+        return client
+
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
