c85105eb4b7ed0a15aee2b371e13aa69b62a8e47
Jon Clayton <m87tech@gmail.com>
2021-02-20 18:18:29 +0000

    more work on zt - name and description now working

---
+++ plugins/modules/zt_set_description.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+
+from __future__ import absolute_import, division, print_function
+
+import requests
+
+__metaclass__ = type
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.basic import *
+
+
+def run_module():
+    # define available arguments/parameters a user can pass to the module
+    module_args = dict(
+        token=dict(type="str", required=True),
+        zt_network_id=dict(type="str", required=True),
+        zt_node_id=dict(type="str", required=True),
+        zt_description=dict(type="str", required=True),
+    )
+
+    # seed the result dict in the object
+    # we primarily care about changed and state
+    # changed is if this module effectively modified the target
+    # state will include any data that you want your module to pass back
+    # for consumption, for example, in a subsequent task
+    result = dict(changed=False, original_message="", message="", data={})
+
+    # the AnsibleModule object will be our abstraction working with Ansible
+    # this includes instantiation, a couple of common attr would be the
+    # args/params passed to the execution, as well as if the module
+    # supports check mode
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
+
+    # if the user is working with this module in only check mode we do not
+    # want to make any changes to the environment, just return the current
+    # state with no modifications
+    if module.check_mode:
+        module.exit_json(**result)
+
+    # manipulate or modify the state as needed (this is going to be the
+    # part where your module will do what it needs to do)
+    # reinstall_servers
+    result["data"] = set_description(module)
+
+    # in the event of a successful module execution, you will want to
+    # simple AnsibleModule.exit_json(), passing the key/value results
+    module.exit_json(**result)
+
+
+def set_description(module):
+
+    token = module.params["token"]
+    zt_network_id = module.params["zt_network_id"]
+    zt_node_id = module.params["zt_node_id"]
+    description = module.params["zt_description"]
+
+    bearer = f"bearer {token}"
+    headers = {"Content-Type": "application/json", "Authorization": bearer}
+    url = f"https://my.zerotier.com/api/network/{zt_network_id}/member/{zt_node_id}"
+
+    # {% set script_final_line = "'{\"config\": {\"ipAssignments\": [\"" + hostvars.zt_ip +"\"]} }'" %}
+    json_payload = {"description": description}
+
+    if token or zt_network_id or zt_node_id is not None:
+        # have to use json.dumps to correctly serialize the json otherwise it complains of "unmarshalled"
+        response = requests.post(url, headers=headers, data=json.dumps(json_payload))
+        data = response.json()
+        return data
+    return {}
+
+
+def main():
+    run_module()
+
+
+if __name__ == "__main__":
+    main()
