d5c6ee89324ac23574220d1aabaef4c9e0683ff2
Jon Clayton <m87tech@gmail.com>
2021-03-07 07:55:11 +0000

    mods while testing on VM in eve-ng

--- terraform/proxmox/provider.tf
+++ terraform/proxmox/provider.tf
@@ -12,7 +12,7 @@ provider "proxmox" {
   pm_tls_insecure = true
 
   // Credentials here or environment
-  pm_api_url    = "https://10.55.0.20:8006/api2/json"
+  pm_api_url    = "https://10.12.12.100:8006/api2/json"
   pm_user       = "terraform-prov@pve"
   pm_log_enable = true
   pm_log_file   = "terraform-plugin-proxmox.log"
