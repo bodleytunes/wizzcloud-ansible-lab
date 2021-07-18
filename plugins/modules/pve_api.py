690b2d45ce2074547edb601c70558f353d7fde47
Jon Clayton <m87tech@gmail.com>
2021-07-18 08:36:34 +0000

    minor fixes here and there

--- plugins/modules/pve_api.py
+++ plugins/modules/pve_api.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # Copyright: (c) 2019 Robin Elfrink <robin@15augustus.nl>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
